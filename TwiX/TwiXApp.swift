7682a4f8087147be946de51d3c1bd541eb34fa25
Andrew Shustrov <a.shustrov5@gmail.com>
2024-10-17 19:51:23 +0300

    default ios app project

---
+++ TwiX/TwiXApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwiXApp.swift
+//  TwiX
+//
+//  Created by Андрей Шустров on 17.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TwiXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
