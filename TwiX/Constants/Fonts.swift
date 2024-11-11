8a73f42a711bf3d3a1fa5e3c215609f657f4b634
alw <alexander.s.filatov@gmail.com>
2024-11-11 15:38:10 +0300

    intro screen done

---
+++ TwiX/Constants/Fonts.swift
@@ -0,0 +1,16 @@
+//
+//  Fonts.swift
+//  TwiX
+//
+//  Created by Alexander on 11.11.2024.
+//
+
+import Foundation
+
+class Fonts {
+    static let Urbanist_Regular: String = "Urbanist-Regular"
+    static let Urbanist_Light: String = "Urbanist-Light"
+    static let Urbanist_Thin: String = "Urbanist-Thin"
+    static let Urbanist_Bold: String = "Urbanist-Bold"
+    static let Urbanist_Medium: String = "Urbanist-Medium"
+}
---
+++ TwiX/Constants/Strings.swift
@@ -0,0 +1,17 @@
+//
+//  Strings.swift
+//  TwiX
+//
+//  Created by Alexander on 11.11.2024.
+//
+
+import Foundation
+class Strings {
+    public enum Icons {
+        static let vkIconString: String = "vkIcon"
+        static let googleIconString: String = "googleIcon"
+        static let appleIconString: String = "appleIcon"
+        static let guestIconString: String = "guestIcon"
+    }
+}
+
