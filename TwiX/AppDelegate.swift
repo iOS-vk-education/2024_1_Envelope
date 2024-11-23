5ef199748d05396d9bfda73fb0aa83ac8492d53a
Andrew Shustrov <a.shustrov5@gmail.com>
2024-11-23 23:24:40 +0300

    save before upgrade

--- TwiX/AppDelegate.swift
+++ TwiX/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SwiftUI
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
+        
+        FirebaseApp.configure()
         
         let rootView = AuthenticationFlowUIView()
         window.rootViewController = UIHostingController(rootView: rootView)
