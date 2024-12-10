e30dd93198e4e8d264bf74fd00ed0360a0a9d90f
alw <alexander.s.filatov@gmail.com>
2024-12-10 17:09:06 +0300

    added new icon, bug with back arrow doesn't fixed

--- TwiX/Navigation/Controller/TabBarController.swift
+++ TwiX/Navigation/Controller/TabBarController.swift
@@ -25,13 +25,13 @@ class TabBarController: UITabBarController {
     
     private func setupTabBarAppearance() {
         tabBar.backgroundColor = Colors.backgroundColor
-        tabBar.tintColor = UIColor(named: "LightPeach")
+        
         tabBar.isTranslucent = false
     }
     
     private func createNav(title: String, image: UIImage?, vc: UIViewController) -> UINavigationController {
         let nav = UINavigationController(rootViewController: vc)
-        
+
         nav.tabBarItem.title = title
         nav.tabBarItem.image = image
         
